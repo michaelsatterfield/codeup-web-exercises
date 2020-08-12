6e10ce033a0d116fe96ade0e5673b2554bbfd37f
michaelsattefield <michael.f.satterfield@gmail.com>
2020-08-12 10:06:20 -0500

    finished functions exercise

---
+++ js/js-functions.js
@@ -0,0 +1,66 @@
+happyBirthday("Ryan")
+happyBirthday("Chris")
+
+ function happyBirthday(name){
+    console.log("Happy Birthday to You")
+    console.log("Happy Birthday to You")
+    console.log("Happy Birthday dear" + name)
+    console.log("Happy Birthday to You!")
+
+}
+
+function favcolor() {
+    var question = "what is fav color";
+    var response = prompt(question);
+    alert
+}
+favcolor();
+favcolor();
+favcolor();
+
+function questionanswer(){
+    var question = "wht do you think about that?";
+    var response = prompt(question);
+    alert(response + " is what i though as well");
+
+}
+
+questionanswer();
+questionanswer();
+
+define a function and immediately invoke (i.e. call) it
+
+
+
+alert(iffeVar); // undefined
+
+var color = "red"
+ favstyle = function() {
+    var question = "wht do you think about stuff?";
+    var response = prompt(question);
+    alert(response + " " + color +  " thatz i thought as well");
+
+}
+favstyle()
+
+
+var x = 300;
+var y = 100;
+
+function scopeExample() {
+    var x = 1;
+    var y = 2;
+    console.log('x: ' + x + ' y: ' + y); // x: 1 y: 2
+    return x + y;
+}
+
+console.log('x: ' + x + ' y: ' + y); // x: 300 y: 100
+var returnValue = scopeExample() + x;
+console.log(returnValue); // 3
+
+function joe(){
+    var x = 4;
+    var b = 12;
+    console.log
+}
+
