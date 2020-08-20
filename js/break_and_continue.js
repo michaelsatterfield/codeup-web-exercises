d4653d0f2000d532c9464d56f6e89689f21052a4
michaelsattefield <michael.f.satterfield@gmail.com>
2020-08-20 14:42:07 -0500

    last of missing files, i hope

---
+++ js/break_and_continue.js
@@ -0,0 +1,21 @@
+var random = Math.floor((Math.random()*50)+1);
+
+console.log("random odd number to skip is: " + random);
+
+for (var i = 1; i < 50; i++) {
+
+    if (i % 2 === 0){
+        //skip the even numbers.
+        continue;
+    }
+
+    if (random === i) {
+        console.log("yikes! skipping number: " + i);
+    } else {
+        console.log("here is an odd number: " +i);
+    }
+    if (i >= 49) {
+        break;
+    }
+
+}
