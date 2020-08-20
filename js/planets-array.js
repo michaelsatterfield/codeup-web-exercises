37a500307f5aec332a01ac2ab9a51eedcb39f1b7
michaelsattefield <michael.f.satterfield@gmail.com>
2020-08-20 15:05:55 -0500

    new Planet arrays

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,50 +1,42 @@
 (function(){
-    
+    "use strict";
 
-    // TODO: Create an array holding the names of the eight planets in our solar system in order, starting closest to the sun.
-    var planets = ['Mercury', 'Venus', 'Earth', 'Mars','Jupiter','Saturn','Uranus', 'Neptune'];
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
 
-    // function for logging the planets array
-    function logPlanets() {
-        console.log("Here is the list of planets:");
-        console.log(planets);
-        
-    }
-
-    logPlanets();
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-        planets.unshift("The Sun");
-    logPlanets();
+    console.log(planets.unshift("The Sun"))
 
     console.log('Adding "Pluto" to the end of the planets array.');
-        planets.push("Pluto");
-    logPlanets();
+    console.log(planets.push("Pluto"));
+
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-        planets.shift();
-    logPlanets();
+    console.log(planets.shift());
 
     console.log('Removing "Pluto" from the end of the planets array.');
-        planets.pop();
-    logPlanets();
+    console.log(planets.pop());
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-        var indexOfEarth = planets.indexOf("Earth");
-
-    console.log('Using splice to remove the planet after "Earth".');
-        var planetAfterEarth = planets.splice(indexOfEarth + 1, 1);
-    logPlanets();
-
-    console.log('Using splice to add back the planet after "Earth".');
-        planets.splice(indexOfEarth + 1, 0, planetAfterEarth);
-        logPlanets();
+    console.log(planets.indexOf("Earth"))
 
     console.log("Reversing the order of the planets array.");
-        planets.reverse();
-    logPlanets();
+    console.log(planets.reverse());
 
     console.log("Sorting the planets array.");
-        planets.sort();
-    logPlanets();
+    console.log(planets.sort());
 })();
