bb8537f1a67e87d95da080b465399780dd65e588
michaelsattefield <michael.f.satterfield@gmail.com>
2020-08-25 10:39:33 -0500

    fizzbuzz

--- js/circle.js
+++ js/circle.js
@@ -26,7 +26,7 @@
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + roundRadius + ", is: " );
+            console.log("Area of a circle with radius: " +this.radius + ", is: "+ Math.round(this.getArea()));
         }
     };
 
