1a477e17d4566f2176a8389050600e716f3cf6a8
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-01-22 15:41:35 -0600

    praticing with fizzbuzz sausage

--- js/PUSH UPS.js
+++ js/PUSH UPS.js
@@ -1,18 +1,35 @@
 
 //fizz buzz
 
-function fizzbuzz(num){
-    for(var i = 1; i <= num; i++){
-        if(i % 15 === 0) {
-            console.log("fizzbuzz");
-        } else if (i % 3 === 0) {
-           console.log("fizz")
-        } else if(i % 5 === 0) {
-            console.log("buzz")
-        } else {
-            console.log(i);
-        }
-    }
-}
+// function fizzbuzz(num){
+//     for(var i = 1; i <= num; i++){
+//         if(i % 15 === 0) {
+//             console.log("fizzbuzz");
+//         } else if (i % 3 === 0) {
+//            console.log("fizz")
+//         } else if(i % 5 === 0) {
+//             console.log("buzz")
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
 
-fizzbuzz(45)
+// fizzbuzz(45)
+
+// function sausageTimer(sausages){
+//     const sausageTalk = document.getElementsByClassName('sausagesDeclaration');
+//     console.log(sausageTalk);
+//     for(let i = 300; i > sausages; i--){
+//         console.log(i)
+//         if(i % 15 === 0){
+//             // sausageTalk.innerHTML = "Hello Codeup!";
+//         } else if(i % 5 === 1){
+//             console.log("you don't have to eat my sausage today.")
+//         } else{
+//             console.log("Hey!")
+//         }
+//     }
+//
+// }
+// sausageTimer(0)
